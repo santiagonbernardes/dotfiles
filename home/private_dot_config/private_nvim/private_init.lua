82e9071ddb738d91b9ba0768194a02c0555da46d
Santiago Bernardes <santiagobernardes@gmail.com>
2026-07-28 21:27:46 -0300

    adds treesitter

--- home/private_dot_config/private_nvim/private_init.lua
+++ home/private_dot_config/private_nvim/private_init.lua
@@ -3,7 +3,7 @@ vim.loader.enable()              -- Faster startup by caching compiled Lua modul
 vim.g.mapleader        = ' '     -- Maps <space> to leader
 vim.g.maplocalleader   = "\\"    -- Maps localleader to \
 
--- Options
+-- # Options
 vim.opt.mouse          = 'a'     -- Enables mouse
 vim.opt.number         = true    -- Show line number
 vim.opt.relativenumber = true    -- Show relative number 
@@ -33,6 +33,8 @@ vim.opt.spelllang      = "en,pt"       -- Languages to use in spell checking
 vim.pack.add({
   -- ## Colorscheme
   { src = "https://github.com/ray-x/starry.nvim" },
+  -- ## Treesitter
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
 -- ## Plugin configuration
@@ -42,9 +44,74 @@ require('starry').setup({
     background = true, -- transparent background
   },
 })
-vim.cmd("colorscheme habamax")
 
--- Keymaps
+vim.cmd("colorscheme dracula")
+
+-- ## Treesitter
+local parsers = {
+  'bash', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'rust', 'python',
+  'query', 'vim', 'vimdoc', 'ruby',
+}
+require('nvim-treesitter').install(parsers)
+
+
+-- # Helper functions
+local function treesitter_try_attach(buf, lang)
+  -- Check if a parser exists and load it
+  if not vim.treesitter.language.add(lang) then return end
+  -- Enable syntax highlighting and other treesitter features
+  vim.treesitter.start(buf, lang)
+
+  -- Check if treesitter indentation is available for this language, and if
+  -- so enable it in case there is no indent query, the indentexpr will
+  -- fallback to the vim's built in one
+  local has_indent_query = vim.treesitter.query.get(lang, 'indents') ~= nil
+
+  -- Enable treesitter based indentation
+  if has_indent_query then
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end
+end
+
+-- # Autocommands
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight copied text',
+  group = vim.api.nvim_create_augroup('custom-highlight-yank', { clear = true }),
+  callback = function() vim.hl.on_yank() end,
+})
+
+
+local treesitter = require('nvim-treesitter')
+local available_parsers = treesitter.get_available()
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    -- Treesitter
+    local buf, filetype = args.buf, args.match
+    local language = vim.treesitter.language.get_lang(filetype)
+
+    if not language then return end
+
+    local installed_parsers = treesitter.get_installed('parsers')
+
+    if vim.tbl_contains(installed_parsers, language) then
+      -- Enable the parser if it is already installed
+      treesitter_try_attach(buf, language)
+    elseif vim.tbl_contains(available_parsers, language) then
+      -- If a parser is available in `nvim-treesitter`,
+      -- auto-install it and enable it after the installation is done
+      require('nvim-treesitter').install(language):await(
+        function()
+          treesitter_try_attach(buf, language)
+        end)
+    else
+      -- Try to enable treesitter features in case the parser exists but is not
+      -- available from `nvim-treesitter`
+      treesitter_try_attach(buf, language)
+    end
+  end,
+})
+
+-- # Keymaps
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = "Remove search highlight" })
 vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down (selected lines)" })
 vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up (selected lines)" })
@@ -57,11 +124,3 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
-
--- Autocommands
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight copied text',
-  group = vim.api.nvim_create_augroup('custom-highlight-yank', { clear = true }),
-  callback = function() vim.hl.on_yank() end,
-})
